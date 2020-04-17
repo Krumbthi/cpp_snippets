ea04dc0ca92c722bf30af0eba898fd2d9e7ea5e4
Erik Dubois <erik.dubois@gmail.com>
2020-04-17 23:02:56 +0200

    cb_test folder

---
+++ qt/cb_test/main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <thread>
+#include <chrono>
+#include "../../qt/usbaccesslayer.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    auto usb = std::make_unique<UsbLayer::IUsbAccessLayer>();
+    usb->InitDLL();
+
+    return a.exec();
+}
