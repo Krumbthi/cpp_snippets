7df64e913b0343f11c552f40b0ad5ab428bd4f1f
krumbthi <48240081+Krumbthi@users.noreply.github.com>
2020-04-17 15:11:53 +0200

    test

---
+++ AppLogger.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <chrono>
+#include <string>
+#include <memory>
+#include <fstream>
+#include <map>
+
+using namespace std::chrono;
+
+#define SPDLOG_LEVEL_TRACE 		0
+#define SPDLOG_LEVEL_DEBUG 		1
+#define SPDLOG_LEVEL_INFO 		2
+#define SPDLOG_LEVEL_WARN 		3
+#define SPDLOG_LEVEL_ERROR 		4
+#define SPDLOG_LEVEL_CRITICAL 	5
+#define SPDLOG_LEVEL_OFF 		6
+
+// Log level enum
+namespace level {
+enum class level_enum
+{
+    trace = SPDLOG_LEVEL_TRACE,
+    debug = SPDLOG_LEVEL_DEBUG,
+    info = SPDLOG_LEVEL_INFO,
+    warn = SPDLOG_LEVEL_WARN,
+    err = SPDLOG_LEVEL_ERROR,
+    critical = SPDLOG_LEVEL_CRITICAL,
+    off = SPDLOG_LEVEL_OFF,
+    n_levels
+};
+}
+
+namespace Logging {
+
+class AppLogger
+{
+public:
+	AppLogger() = default;
+	
+	AppLogger(const AppLogger&) = delete;
+	AppLogger& operator=(const AppLogger&) = delete;
+	AppLogger(AppLogger&&) = delete;
+	AppLogger& operator=(AppLogger&&) = delete;
+
+	~AppLogger();
+	
+    void		SetTag(std::string tag);
+    void 		SetLogFile(std::string fileName);
+	
+    void        Msg(const std::string fmt, ...);
+    void        Msg(const char* _log, ...);
+
+private:
+    milliseconds    GetTimestamp() const;
+    bool            FileLogging;
+    std::ofstream	LogFile;
+    std::string		Tag;
+};
+}
---
+++ usbaccesslayer.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <QObject>
+#include <memory>
+
+#ifdef Q_OS_WIN
+#include "windows.h"
+#endif
+
+#include "AppLogger.h"
+#include "FrameDispatcher.h"
+//#include "devicechangenotifier.h"
+#include "ErrorHandling.h"
+
+namespace UsbLayer {
+
+using namespace Data;
+
+class IUsbAccessLayer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit IUsbAccessLayer(QObject* parent=nullptr);
+    ~IUsbAccessLayer();
+    void registerNotifyer();
+
+    void InitDLL();
+    void RegisterForBFChange(HWND winHandle, quint8 &noOfConDevs);
+    void CloseDll();
+
+    void ReadBfMainVerison();
+    void ReadROStatus();
+    void ReadBFStatus();
+    void ReadBFMainVerion();
+
+    void newDeviceDetected(quint8 dev);
+
+protected:
+     //void closeEvent(QCloseEvent *event);
+     //bool nativeEvent(const QByteArray& eventType, void* message, long* result);
+
+private:
+    void ParseErrorMessage(UsbResult errVal);
+    void DispatchDataFrame(uint8_t* dataFrame, size_t frameSize);
+    void Sleep(int val);
+
+    //Ui::MainWindow *ui;
+    std::unique_ptr<Logging::AppLogger> Logger;
+    std::unique_ptr<FrameDispatcher>    FrameDisp;
+    //DeviceChangeNotifier*                DevNotifier;
+    quint8                              NoOfConnectedDevs = 0;
+};
+
+}
